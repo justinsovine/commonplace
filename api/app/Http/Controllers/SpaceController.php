454d1b7027f1d344a28a4f83c38fa0458deef445
Justin Sovine <justinsovine@gmail.com>
2025-04-19 18:31:37 -0400

    Cleanup and comments

--- api/app/Http/Controllers/SpaceController.php
+++ api/app/Http/Controllers/SpaceController.php
@@ -7,36 +7,18 @@ use Illuminate\Http\Request;
 
 class SpaceController extends Controller
 {
-    // convert this to model
-    private $listOfSpaces = [
-        [
-            'img' => '/img/test.jpg',
-            'name' => 'Eastwood Room (Performance Hall)',
-            'description' => 'A 75-seat black box theater with flexible staging. Ideal for performances, speaker events, film screenings, and rehearsals. Includes basic AV setup, dimmable lighting, and stackable chairs for reconfiguration.',
-        ],
-        [
-            'img' => '/img/test.jpg',
-            'name' => 'Loft Studio (Art & Workshop Space)',
-            'description' => 'Located on the top floor with ample skylights and industrial sinks. Designed for hands-on workshops, art classes, pottery, or fiber arts. Equipped with folding tables, easels, and open storage for short-term material use.',
-        ],
-        [
-            'img' => '/img/test.jpg',
-            'name' => 'Linden Room (Meeting & Multi-Use Space)',
-            'description' => 'A clean, multi-purpose space with whiteboard walls and stackable furniture. Used for public lectures, book clubs, civic meetings, small classes, and tutoring. Quiet, well-lit, and adjacent to restrooms and a kitchenette.',
-        ],
-    ];
-
     public function index(Request $request) {
         return response()->json([
             'message' => 'List of all available spaces',
-            'spaces' => $this->listOfSpaces,
+            'spaces' => Space::all(),
         ]);
     }
 
-    public function show(Request $request, int $space) {
+    // Uses "route model binding" to inject a model into controller method based on id in the route
+    public function show(Request $request, Space $space) {
         return response()->json([
             'message' => 'Details about a single space',
-            'id' => $space, // convert this to Eloquent binding
+            'space' => $space,
         ]);
     }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SpaceController;
 
+// Get all spaces
 Route::get('/spaces', [SpaceController::class, 'index']);
 
+// Get a single space by ID
 Route::get('/spaces/{space}', [SpaceController::class, 'show']);
