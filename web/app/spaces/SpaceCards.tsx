9d07c37a5c77e06c4d666e333137ee10ebde8d32
Justin Sovine <justinsovine@gmail.com>
2025-04-24 21:03:33 -0400

    Modified spacecards api reference

--- web/app/spaces/SpaceCards.tsx
+++ web/app/spaces/SpaceCards.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Space } from '../../src/services/api';
+import { Space } from '@/api';
 
 export default function SpaceCards({ spaces }: { spaces: Space[] }) {
     const cardsRef = useRef<HTMLDivElement>(null);
